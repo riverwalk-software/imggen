3b3aeb11f71ffd85a36303ef1425e4ed3ebd73b6
wkenned1 <wkenned1@binghamton.edu>
2025-01-24 16:30:24 +0000

    Test extension endpoint

--- src/index.ts
+++ src/index.ts
@@ -15,31 +15,47 @@ const app = new Hono<{ Bindings: Bindings }>();
 await initWasm(wasmModule);
 
 app.get('/', zValidator('query', QueryParametersSchema), async (c) => {
-	const queryParameters = c.req.valid('query');
-	const { configuration, fontFamily, fontVariant, layout, layoutIndex } = queryParameters;
-	const parsedQueryParamaters = LayoutSchema.parse({
-		discriminator: `${layout}${layoutIndex.toString()}`,
-		data: queryParameters,
-	});
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-	const markup = createMarkup(parsedQueryParamaters);
-	const vector = await markupToVector(configuration, fontFamily, fontVariant, c.env.GOOGLE_FONTS, markup);
-	const raster = vectorToRaster(vector);
-	return c.body(raster, 200, {
-		'Content-Type': 'image/png',
-	});
+  const queryParameters = c.req.valid('query');
+  const { configuration, fontFamily, fontVariant, layout, layoutIndex } = queryParameters;
+  const parsedQueryParamaters = LayoutSchema.parse({
+    discriminator: `${layout}${layoutIndex.toString()}`,
+    data: queryParameters,
+  });
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const markup = createMarkup(parsedQueryParamaters);
+  const vector = await markupToVector(configuration, fontFamily, fontVariant, c.env.GOOGLE_FONTS, markup);
+  const raster = vectorToRaster(vector);
+  return c.body(raster, 200, {
+    'Content-Type': 'image/png',
+  });
+});
+
+app.get('/image\.png', zValidator('query', QueryParametersSchema), async (c) => {
+  const queryParameters = c.req.valid('query');
+  const { configuration, fontFamily, fontVariant, layout, layoutIndex } = queryParameters;
+  const parsedQueryParamaters = LayoutSchema.parse({
+    discriminator: `${layout}${layoutIndex.toString()}`,
+    data: queryParameters,
+  });
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const markup = createMarkup(parsedQueryParamaters);
+  const vector = await markupToVector(configuration, fontFamily, fontVariant, c.env.GOOGLE_FONTS, markup);
+  const raster = vectorToRaster(vector);
+  return c.body(raster, 200, {
+    'Content-Type': 'image/png',
+  });
 });
 
 app.onError((error, c) => {
-	if (error instanceof z.ZodError) {
-		return c.json({ error: error.issues }, 400);
-	} else if (error instanceof HTTPException) {
-		return c.json({ error: error.message }, error.status);
-	} else if (error instanceof Error) {
-		return c.json({ error: error.message }, 500);
-	} else {
-		return c.json({ error: 'Unknown error' }, 500);
-	}
+  if (error instanceof z.ZodError) {
+    return c.json({ error: error.issues }, 400);
+  } else if (error instanceof HTTPException) {
+    return c.json({ error: error.message }, error.status);
+  } else if (error instanceof Error) {
+    return c.json({ error: error.message }, 500);
+  } else {
+    return c.json({ error: 'Unknown error' }, 500);
+  }
 });
 
 export default app;
