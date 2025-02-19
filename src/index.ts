e97b5f4bfbab12afa3a9d0a1187ea205f812bf06
wkenned1 <wkenned1@binghamton.edu>
2025-02-19 03:25:44 +0000

    Add svg storage and call png api

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,42 @@ import { initWasm } from '@resvg/resvg-wasm';
 import wasmModule from './binaries/resvg.wasm';
 import markupToVector from './lib/markupToVector';
 import vectorToRaster from './lib/vectorToRaster';
-import { z } from 'zod';
+import { string, z } from 'zod';
 import createMarkup from './lib/createMarkup.js';
 import { zValidator } from '@hono/zod-validator';
 import { QueryParametersSchema } from './schemas/queryParameters';
 import { HTTPException } from 'hono/http-exception';
 import { LayoutSchema } from './schemas/layouts';
+import { DurableObject } from "cloudflare:workers";
+
+export class StoredImage extends DurableObject {
+
+  constructor(ctx: DurableObjectState, env: Env) {
+    super(ctx, env);
+  }
+
+  async putImage(value: string): Promise<string> {
+    console.log("working 5")
+    const existingImage: string | undefined = await this.ctx.storage.get("image");
+    console.log("working 6")
+
+    if (existingImage) {
+      return existingImage;
+    }
+
+    await this.ctx.storage.put("image", value);
+
+    return value;
+  }
+
+  async getImage(): Promise<string | undefined> {
+    return this.ctx.storage.get("image");
+  }
+
+  async deleteImage(): Promise<void> {
+    await this.ctx.storage.delete("image");
+  }
+}
 
 export interface Env {
   IMAGE_QUEUE_PUBLISH: Queue<any>;
@@ -45,6 +75,8 @@ app.get('/', zValidator('query', QueryParametersSchema), async (c) => {
 });
 
 app.post('/', zValidator('query', QueryParametersSchema), async (c) => {
+  const userId = "wkenend1";
+
   const queryParameters = c.req.valid('query');
   const { configuration, fontFamily, fontVariant, layout, layoutIndex } = queryParameters;
   const parsedQueryParamaters = LayoutSchema.parse({
@@ -57,7 +89,19 @@ app.post('/', zValidator('query', QueryParametersSchema), async (c) => {
 
   const vector = await markupToVector(configuration, fontFamily, fontVariant, c.env.GOOGLE_FONTS, markup);
 
-  // await c.env.IMAGE_QUEUE_PUBLISH.send({ userData: { username: "wkenned1" }, imageMetadata: parsedQueryParamaters, image: vector });
+  const imageKey = `${userId}:${c.req.url.split('?')[1]}`;
+
+  console.log("working1");
+  const imageId = c.env.SVG_CACHE.idFromName(imageKey);
+  console.log("working2");
+
+  const obj = c.env.SVG_CACHE.get(imageId);
+
+  console.log("working3");
+
+  await obj.putImage(vector);
+
+  await c.env.IMAGE_QUEUE_PUBLISH.send({ userData: { username: "wkenned1" }, imageMetadata: parsedQueryParamaters, imageKey: imageKey });
 
   return c.body(vector, 200, {
     'Content-Type': 'image/svg+xml',
@@ -77,4 +121,28 @@ app.onError((error, c) => {
   }
 });
 
-export default app;
+export default {
+  fetch: app.fetch,
+  async queue(batch: MessageBatch<any>, env: Environment) {
+    console.log(`MESSAGE: ${JSON.stringify(batch.messages[0].body)}`);
+    const body = batch.messages[0].body;
+
+    const imageId = env.SVG_CACHE.idFromName(body.imageKey);
+    const obj = env.STRING_STORE.get(imageId);
+
+    const image = await obj.getImage();
+
+    const encoder = new TextEncoder();
+    const imageData = encoder.encode(image);
+
+    const response = await fetch("http://localhost:3000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "image/svg+xml", // Change if needed (e.g., "image/png")
+      },
+      body: imageData, // Send the raw binary data
+    });
+
+    console.log(`RESPONSE: ${response.status}`);
+  },
+}
