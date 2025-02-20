19050ccfbf995c73152d0829c00b5796f2de52ff
wkenned1 <wkenned1@binghamton.edu>
2025-02-19 21:58:32 -0500

    Clean up logs

--- src/index.ts
+++ src/index.ts
@@ -15,62 +15,6 @@ export interface Env {
   IMAGE_QUEUE_PUBLISH: Queue<any>;
 }
 
-// export class StoredImage extends DurableObject {
-
-//   constructor(ctx: DurableObjectState, env: Env) {
-//     super(ctx, env);
-//   }
-
-//   async putImage(value: string): Promise<string> {
-//     const existingImage: string | undefined = await this.ctx.storage.get("image");
-
-//     if (existingImage) {
-//       return existingImage;
-//     }
-
-//     await this.ctx.storage.put("image", value);
-
-//     return value;
-//   }
-
-//   async getImage(): Promise<string | undefined> {
-//     return this.ctx.storage.get("image");
-//   }
-
-//   async deleteImage(): Promise<void> {
-//     await this.ctx.storage.delete("image");
-//   }
-// }
-
-// export class StoredPng extends DurableObject {
-
-//   constructor(ctx: DurableObjectState, env: Env) {
-//     super(ctx, env);
-//   }
-
-//   async putImage(value: Uint8Array): Promise<Uint8Array> {
-//     const existingImage: Uint8Array | undefined = await this.ctx.storage.get("image");
-
-//     if (existingImage) {
-//       return existingImage;
-//     }
-
-//     await this.ctx.storage.put("image", value);
-
-//     return value;
-//   }
-
-//   async getImage(): Promise<Uint8Array | undefined> {
-//     return this.ctx.storage.get("image");
-//   }
-
-//   async deleteImage(): Promise<void> {
-//     await this.ctx.storage.delete("image");
-//   }
-// }
-
-
-
 const app = new Hono<{ Bindings: Bindings }>().basePath('/api/snapgen');
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 await initWasm(wasmModule);
@@ -114,23 +58,13 @@ app.post('/', zValidator('query', QueryParametersSchema), async (c) => {
 
   const imageKey = `${userId}:${c.req.url.split('?')[1]}`;
 
-  // console.log("working1");
-  // const imageId = c.env.SVG_CACHE.idFromName(imageKey);
-  console.log("working2");
-
-  // const obj = c.env.SVG_CACHE.get(imageId);
-
   if (!c.env.SVG_BUCKET) {
     console.error("SVG_BUCKET is not defined in the environment");
   }
 
   await c.env.SVG_BUCKET.put(imageKey, vector);
 
-  console.log("working3");
-
   await c.env.IMAGE_QUEUE_PUBLISH.send({ userData: { username: "wkenned1" }, imageMetadata: parsedQueryParamaters, imageKey: imageKey });
-
-  console.log("working5");
 
   return c.body(vector, 200, {
     'Content-Type': 'image/svg+xml',
@@ -152,25 +86,16 @@ app.get('/image', async (c) => {
 
   const body = await response.json();
 
-  console.log("PULL RESPONSE");
-  console.log(body)
-
   if (body.result.messages.length === 0) {
     return c.json({ error: 'No messages' }, 404);
   }
 
   const message = JSON.parse(body.result.messages[0].body);
 
-  console.log(message)
-
   const imageKey = message.imageKey;
 
   const image = await c.env.PNG_BUCKET.get(imageKey);
   const imageBinary = await streamToUint8Array(image.body);
-
-  console.log(imageBinary);
-
-  console.log(`IMAGE SIZE: ${imageBinary.byteLength}`)
 
   return c.body(imageBinary, 200, {
     'Content-Type': 'image/png',
@@ -222,70 +147,34 @@ export default {
     console.log(`MESSAGE: ${JSON.stringify(batch.messages[0].body)}`);
     const body = batch.messages[0].body;
 
-    console.log("WORKING WORKING WORKING")
+    if (!env.SVG_BUCKET) {
+      console.error("PNG_BUCKET is not defined in the environment");
+    }
 
-    // const imageId = env.SVG_CACHE.idFromName(body.imageKey);
-    // const obj = env.SVG_CACHE.get(imageId);
-    // const image = await obj.getImage();
     const image = await env.SVG_BUCKET.get(body.imageKey);
 
-    // console.log("TESIING 1")
-    // console.log(image);
+    const response = await fetch("https://snapgen.media/png/", {
+      method: "POST",
+      headers: {
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        "Content-Type": "image/svg+xml",
+        "CF-Worker": "true",
+        "User-Agent": "SnapGen/1.0 (Cloudflare Worker; +https://staticpress.host)"
+      },
+      body: await streamToUint8Array(image.body),
+    });
 
-    // const encoder = new TextEncoder();
-    // const imageData = encoder.encode(image);
-
-    console.log("TESIING 2")
-
-    try {
-      // console.log(imageData.byteLength);
-
-      const response = await fetch("https://snapgen.media/png/", {
-        method: "POST",
-        headers: {
-          "Accept": "*/*",
-          "Connection": "keep-alive",
-          "Content-Type": "image/svg+xml", // Change if needed (e.g., "image/png")
-          "CF-Worker": "true",
-          "User-Agent": "SnapGen/1.0 (Cloudflare Worker; +https://staticpress.host)"
-        },
-        body: await streamToUint8Array(image.body), // Send the raw binary data
-      });
-
-      console.log(`RESPONSE: ${response.status} ${response.statusText}`);
-
-      console.log("TESIING 3")
-
-
-      // const pngKey = `png:${body.imageKey}`;
-
-      // const pngId = env.PNG_CACHE.idFromName(pngKey);
-      // const pngObj = env.PNG_CACHE.get(pngId);
-      // const png = await pngObj.setImage(new Uint8Array(await response.arrayBuffer()));
-      const pngBody = await response.arrayBuffer();
-      console.log(pngBody);
-
-      // console.log(`PNG size: ${pngBody.byteLength}`);
-
-      if (!env.PNG_BUCKET) {
-        console.error("SVG_BUCKET is not defined in the environment");
-      }
-
-      const res = await env.PNG_BUCKET.put(body.imageKey, pngBody, {
-        httpMetadata: { contentType: "image/png" },
-      });
-
-      console.log(`RES: ${res}`);
-
-      console.log("TESIING 4")
-
-      await env.PNG_QUEUE_PUBLISH.send({ userData: { username: "wkenned1" }, imageMetadata: body.imageMetadata, imageKey: body.imageKey });
-
-      console.log("TESIING 5")
-    }
-    catch (error) {
-      console.error("Error processing image:", error);
+    if (!env.PNG_BUCKET) {
+      console.error("PNG_BUCKET is not defined in the environment");
     }
 
+    const pngBody = await response.arrayBuffer();
+
+    await env.PNG_BUCKET.put(body.imageKey, pngBody, {
+      httpMetadata: { contentType: "image/png" },
+    });
+
+    await env.PNG_QUEUE_PUBLISH.send({ userData: { username: "wkenned1" }, imageMetadata: body.imageMetadata, imageKey: body.imageKey });
   },
 }
